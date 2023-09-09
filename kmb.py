3d2f66086cd958d1927582e3a1f9db448f0fe357
kimicbum <144503086+kimicbum@users.noreply.github.com>
2023-09-09 11:39:39 +0900

    Update kmb.py

--- kmb.py
+++ kmb.py
@@ -1,4 +1,10 @@
 import streamlit as st
+font_dirs = [os.getcwd() + '/customFonts']
+font_files = fm.findSystemFonts(fontpaths=font_dirs)
+
+for font_file in font_files:
+    fm.fontManager.addfont(font_file)
+fm._load_fontmanager(try_read_cache=False)
 
 st.title('RIOT GAMES 소개')
 image_path= 'rgg.PNG'
